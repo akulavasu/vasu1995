7baea044a563411ddd57898060cd51a58e584075
vakula <vakula@GIVSARDIN46541.sapient.com>
2017-12-22 16:09:44 +0530

    quiz demo

---
+++ QuizDemo/src/com/sapient/dao/ExamDaoImpl.java
@@ -0,0 +1,51 @@
+package com.sapient.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import static com.sapient.util.ExamUtil.*;
+
+import com.sapient.util.ExamUtil;
+import com.sapient.vo.Question;
+
+public class ExamDaoImpl implements Idao {
+	private static Idao dao=new ExamDaoImpl();
+	private ExamDaoImpl(){
+		
+	}
+	public static Idao getDaoInstance(){
+		return dao;
+	}
+	static{
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Map<Integer, Question> viewQuestions() {
+		Map<Integer,Question> map=new HashMap<>();
+		Question ques=null;
+		String url=URL;
+		try(Connection conn=DriverManager.getConnection(url,USERNAME,PWD )){
+			String sql="select *from questions";
+			PreparedStatement st=conn.prepareStatement(sql);
+			ResultSet rs=st.executeQuery(sql);
+			while(rs.next()){
+				ques=new Question((rs.getInt("qid")),rs.getString("qdesc"),rs.getString("optiona"),rs.getString("optionb"),rs.getString("optionc"),rs.getString("answer"));
+				map.put(ques.getQid(),ques );
+			}
+		
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return map;
+	}
+
+}
---
+++ QuizDemo/src/com/sapient/dao/Idao.java
@@ -0,0 +1,10 @@
+package com.sapient.dao;
+
+import java.util.Map;
+
+import com.sapient.vo.Question;
+
+public interface Idao {
+	Map<Integer,Question> viewQuestions();
+
+}
